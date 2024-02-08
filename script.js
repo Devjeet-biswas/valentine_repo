c3ec30d33d37f2805c27accb9f36866ae2048de9
Devjeet Biswas <104926391+Devjeet-biswas@users.noreply.github.com>
2024-02-08 19:33:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function respondToInvitation(accepted) {
+    if (accepted) {
+        window.location.href = 'questionnaire.html';
+    } else {
+        // You can handle different behavior if she declines
+        window.location.href = 'rejection.html';
+    }
+}
+
+
+function respondToInvitation2(accepted) {
+    if (accepted) {
+        window.location.href = 'questionnaire.html';
+    } else {
+        // You can handle different behavior if she declines
+        alert("Gandi ladki of the universe🥹👍🏼");
+    }
+}
